2330d9e4e6a91a17077e287f9e4d08d5690874f6
Tucker Gillbrand <tucker@gillbrand.com>
2021-07-29 09:14:54 -0700

    Initial Commit

---
+++ APIHackwich/APIHackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIHackwichApp.swift
+//  APIHackwich
+//
+//  Created by Tucker(School) on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct APIHackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
